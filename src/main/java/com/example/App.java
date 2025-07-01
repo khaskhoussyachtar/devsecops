e50713b1dd38ec5bcbb3287ceae9458348b8b867
khaskhoussyachtar <achtar.khaskhoussy@esprit.tn>
2025-07-01 17:09:28 +0000

    Ajout du code source Java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins + Maven!");
+        System.out.println("Date système : " + new java.util.Date());
+    }
+}
