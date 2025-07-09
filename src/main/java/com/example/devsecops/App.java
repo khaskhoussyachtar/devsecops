1224fbc1a73a1e050f659257bf9d7d8b67cd23f0
achtar <achtarkhaskhoussy@esprit.tn>
2025-07-09 22:36:48 +0000

    Initial commit

---
+++ src/main/java/com/example/devsecops/App.java
@@ -0,0 +1,13 @@
+package com.example.devsecops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/example/devsecops/AppTest.java
@@ -0,0 +1,13 @@
+package com.example.devsecops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testAppWorks() {
+        assertTrue(true);
+    }
+}
+
